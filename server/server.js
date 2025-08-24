25f1da4f0d2a297e43dd41984bcfe037a1927c3a
Vishesh Mishra <mishravishesh1403@gmail.com>
2025-08-24 23:34:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["https://url-shortener-rho-six.vercel.app/"], 
+  origin: ["https://url-shortener-rho-six.vercel.app"], 
   methods: ["GET", "POST"],
   credentials: true
 })); ;
